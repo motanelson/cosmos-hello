3935c6dc90888c03dbec2e6119f5087e30a71837
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2022-12-11 14:46:42 +0000

    Add files via upload

--- Kernel.cs
+++ Kernel.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using System.Text;
 using Sys = Cosmos.System;
 
-namespace CosmosKernel1
+namespace CosmosKernel2
 {
     public class Kernel : Sys.Kernel
     {
 
         protected override void BeforeRun()
         {
-            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("hello world\n.");
         }
 
         protected override void Run()
         {
-            Console.Write("Input: ");
+           
             var input = Console.ReadLine();
-            Console.Write("Text typed: ");
-            Console.WriteLine(input);
+            
+            
         }
     }
 }
